4a597306cef28cc1fcd5e01a401157a7a538cc54
lucas <lsantana1234@hotmail.com>
2023-02-07 11:35:06 -0300

    add: nodemon

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
@@ -13,3 +14,9 @@ app.get("/message/:id/:user", (request, response) =>{
     const {id ,user} = request.params
     response.send(`Id message: ${id} User: ${user}`);
 });
+
+app.get("/users", (request, response) => {
+    const {page, limit} = request.query;
+
+    response.send(`Page: ${page}. Show: ${limit}`);
+})
