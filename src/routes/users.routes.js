1ea52e8235971526ee4956f0dcb28ec5987cc978
lucas <lsantana1234@hotmail.com>
2023-04-29 01:41:30 -0300

    :sparkles: criado a função para upload de imagem

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,25 +1,20 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
-
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
-
-
-// function myMiddleware(request, response, next){
-//     console.log("you went through the middleware");
-
-//    if(!request.body.isAdmin){
-//         return response.json({message: "User unauthorized"})
-//    };
-
-//     next();
-// }
+const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/",ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+    console.log(request.file.filename);
+    response.json();
+});
 
 module.exports = usersRoutes;
